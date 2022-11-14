50b5d4cf676deeb86603f15bd6280674218fd2d3
artemflomaster <77832510+artemflomaster@users.noreply.github.com>
2022-11-14 17:05:31 +0300

    sticky notes started

---
+++ sticky-notes/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+
+export default function App() {
+const test = 'test';
+
+  return (
+    <div className="App">
+      {test}
+      ddddfv
+      dvdvdv
+      dvgr
+      v
+      
+    </div>
+  );
+};
+
+
