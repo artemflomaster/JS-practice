2df207e6674a7215385d22265b07bdee4b964987
artemflomaster <77832510+artemflomaster@users.noreply.github.com>
2022-11-15 12:45:17 +0300

    base functionality created
    add note
    remove note

--- sticky-notes/src/App.js
+++ sticky-notes/src/App.js
@@ -1,18 +1,58 @@
 import React from 'react';
 import './App.css';
+import Note from './Note';
+import { nanoid } from 'nanoid';
 
 
 export default function App() {
-const test = 'test';
+  const [notes, setNotes] = React.useState([
+    {
+      id: '123',
+      header: 'New header',
+      content: 'write something'
+    }
+  ])
+
+
+  function createNote() {
+    setNotes(prev => {
+      console.log('add')
+      return ([...prev,
+      {
+        id: nanoid(),
+        header: 'New header',
+        content: 'write something'
+      }
+      ]
+      )
+    }
+    )
+  }
+
+  function removeNote(id){
+setNotes(prev =>{
+  const filtered = prev.filter(note => note.id!==id);
+return filtered;
+})
+  }
+
+  const content = notes.map(note => {
+    return (
+      <Note key={nanoid()} header={note.header} content={note.content} id={note.id} removeHandler={removeNote} />
+    )
+  }
+  )
+
 
   return (
     <div className="App">
-      {test}
-      ddddfv
-      dvdvdv
-      dvgr
-      v
-      
+      <div className='notes-wrapper'>
+        {content}
+      </div>
+      <div className='footer'>
+        <button onClick={createNote}>Create new note</button>
+      </div>
+
     </div>
   );
 };
---
+++ sticky-notes/src/Note.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Note(props) {
+    return (
+        <div className="note">
+            <div className="note-header">{props.header}<button onClick={() => props.removeHandler(props.id)}>X</button></div>
+            <div className="note-content">{props.content}</div>
+
+        </div>
+    )
+}
