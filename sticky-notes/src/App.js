01b44a07e8363684b0ee73d7182c68f2050614a3
artemflomaster <77832510+artemflomaster@users.noreply.github.com>
2022-11-17 16:30:50 +0300

    full functionality and random colours

--- sticky-notes/src/App.js
+++ sticky-notes/src/App.js
@@ -11,7 +11,8 @@ export default function App() {
     {
       id: '123',
       header: 'New header',
-      content: 'write something'
+      content: 'write something',
+      color: 'color1'
     }
   ])
 
@@ -19,15 +20,34 @@ export default function App() {
     setNotes(JSON.parse(localStorage.getItem('stickynotes')));
     setLoading(true);
     console.log('loaded from local storage')
+
+
+
+
   }, [])
 
 
+
   //save notes to local storage on every state update
-  if (isLoaded){
-  localStorage.setItem('stickynotes', JSON.stringify(notes));
-  // console.log(localStorage.getItem('stickynotes'))
+  if (isLoaded) {
+    localStorage.setItem('stickynotes', JSON.stringify(notes));
+    // console.log(localStorage.getItem('stickynotes'))
+    //
+    //   const textArea = document.getElementsByName('content');
+    //   console.log(textArea.length);
+
+    //   for (let i = 0; i < textArea.length; i++) {
+    //     console.log(textArea[i])
+    //     textArea[i].style.height = textArea[i].scrollHeight+100 + 'px;';
+    //   }
+
   }
 
+  function getColor() {
+    const randomIndex = 1 + Math.floor(Math.random() * 5);
+    return 'color' + randomIndex;
+  }
+  //creates new note
   function createNote() {
     setNotes(prev => {
       console.log('add')
@@ -35,7 +55,8 @@ export default function App() {
       {
         id: nanoid(),
         header: 'New header',
-        content: 'write something'
+        content: 'write something',
+        color: getColor()
       }
       ]
       )
@@ -43,6 +64,8 @@ export default function App() {
     )
   }
 
+
+  //removing note by clickin X on it
   function removeNote(id) {
     setNotes(prev => {
       const filtered = prev.filter(note => note.id !== id);
@@ -50,9 +73,28 @@ export default function App() {
     })
   }
 
+  //form handler. controlling form data in state
+  function formHandler(targetName, targetValue, id) {
+
+    setNotes(prevNotes => {
+      return prevNotes.map(prevNote => {
+        let noteToReturn = {}
+        // console.log(id)
+        //if the id is the same then change value. Else dont change anything
+        prevNote.id === id ? noteToReturn = { ...prevNote, [targetName]: targetValue } : noteToReturn = prevNote;
+        return noteToReturn
+      })
+    })
+
+  }
+
+
+
+
+  //mapping the content
   const content = notes.map(note => {
     return (
-      <Note key={nanoid()} header={note.header} content={note.content} id={note.id} removeHandler={removeNote} />
+      <Note key={note.id} header={note.header} content={note.content} color={note.color} id={note.id} removeHandler={removeNote} formHandler={formHandler} />
     )
   }
   )
--- sticky-notes/src/Note.js
+++ sticky-notes/src/Note.js
@@ -1,10 +1,40 @@
 import React from "react";
 
 export default function Note(props) {
+
+    // let hStyle = {};
+
+    // function heightHandler(e) {
+    //     console.log(e.target.scrollHeight);
+    //     let hStyle = 
+    //     {
+    //         height: e.target.scrollHeight +'px;'
+    //     }
+    // }
+    // style={hStyle} onKeyUp={(e) => heightHandler(e)}
+    // const textArea = document.getElementsByName('content');
+    // console.log(textArea);
+    // textArea.style.height = textArea.scrollHeight + 'px;';
+    // const textArea = document.getElementsByName('content');
+    //     console.log(textArea.length);
+
+    //     for (let i = 0; i < textArea.length; i++) {
+    //       console.log(textArea[i])
+    //       textArea[i].style.height = textArea[i].scrollHeight+100 + 'px;';
+    //     }
+    console.log(props.color);
+    const noteStyle = 'note ' + props.color;
     return (
-        <div className="note">
-            <div className="note-header"><input type='text' value={props.header} /><button onClick={() => props.removeHandler(props.id)}>X</button></div>
-            <div className="note-content"><textarea>{props.content}</textarea></div>
+        <div className={noteStyle}>
+            <div className="note-header">
+                <input className='input-header' name='header' placeholder='header' type='text' value={props.header} onChange={(e) => props.formHandler(e.target.name, e.target.value, props.id)} />
+                <button className="button-header" onClick={() => props.removeHandler(props.id)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="-0 0 20 20"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" /></svg>
+                </button>
+            </div>
+            <div className="note-content">
+                <textarea className='text-content' placeholder="content" name='content' value={props.content} onChange={(e) => props.formHandler(e.target.name, e.target.value, props.id)} />
+            </div>
 
         </div>
     )
