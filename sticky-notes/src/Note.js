9239f0c2efedb144c9e2b2b973288ff8768d57d8
artemflomaster <77832510+artemflomaster@users.noreply.github.com>
2022-11-17 17:15:09 +0300

    styling
    - bg
    - border
    - shadows
    - fonts

--- sticky-notes/src/Note.js
+++ sticky-notes/src/Note.js
@@ -28,9 +28,7 @@ export default function Note(props) {
         <div className={noteStyle}>
             <div className="note-header">
                 <input className='input-header' name='header' placeholder='header' type='text' value={props.header} onChange={(e) => props.formHandler(e.target.name, e.target.value, props.id)} />
-                <button className="button-header" onClick={() => props.removeHandler(props.id)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="-0 0 20 20"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" /></svg>
-                </button>
+                <svg className="button-header" onClick={() => props.removeHandler(props.id)} xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" /></svg>
             </div>
             <div className="note-content">
                 <textarea className='text-content' placeholder="content" name='content' value={props.content} onChange={(e) => props.formHandler(e.target.name, e.target.value, props.id)} />
